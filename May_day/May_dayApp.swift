b885d68f2974c6938a67d2503432fce78ebba38d
ken <>
2022-02-21 10:45:09 +0900

    Initial Commit

---
+++ May_day/May_dayApp.swift
@@ -0,0 +1,17 @@
+//
+//  May_dayApp.swift
+//  May_day
+//
+//  Created by 中川賢亮 on 2022/02/21.
+//
+
+import SwiftUI
+
+@main
+struct May_dayApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
